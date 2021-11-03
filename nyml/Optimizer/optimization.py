daf5221aa3230b82a06b1e7a6d60048381aa427a
Bryan <44324535+BeeGass@users.noreply.github.com>
2021-11-02 21:56:50 -0400

    fixed adagrad, rms-prop, adam and implemented adadelta, adamw

--- nyml/Optimizer/optimization.py
+++ nyml/Optimizer/optimization.py
@@ -42,6 +42,11 @@ class optimization():
             data_rand = data[p]
             for i in np.arange(0, data.shape[0], size_of_batch):
                 yield data_rand[i:i + size_of_batch]
+
+    # ----------------------------------------------------------------------------------------------------------------------
+    # ----------------------------------------------- First Order Algorithms -----------------------------------------------
+    # ----------------------------------------------------------------------------------------------------------------------
+    
     
     """
     By taking the gradients of all the points within the landscape, iteratively update parameters in order to minimize (descend) the loss function (within the loss function's landscape).  
@@ -60,10 +65,10 @@ class optimization():
     """    
     def gradient_descent(self, params:dict, lr:float, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict:
         size_of_batch = len(data) # batch size is equal to the size of the dataset
-        d_param = {}
 
         for i in range(epochs):
             for scrambled_dataset in self.batch_iterator(data, size_of_batch):
+                d_param = {}
                 d_param = self.eval_grads(loss_fun, params, scrambled_dataset)
                 for param in params:
                     params[param] = params[param] - (lr * d_param[param]) # e.g. w = w - lr * d_w
@@ -88,10 +93,10 @@ class optimization():
     """ 
     def stochastic_gradient_descent(self, params:dict, lr:float, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict:
         size_of_batch = 1
-        d_param = {}
 
         for i in range(epochs):
             for random_data_point in self.batch_iterator(data, size_of_batch):
+                d_param = {}
                 d_param = self.eval_grads(loss_fun, params, random_data_point)
                 for param in params:
                     params[param] = params[param] - (lr * d_param[param]) # e.g. w = w - lr * d_w
@@ -115,9 +120,9 @@ class optimization():
         <dict>: The optimized parameters
     """ 
     def mini_batch_gradient_descent(self, params:dict, lr:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict:
-        d_param = {}
         for i in range(epochs):
             for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
                 d_param = self.eval_grads(loss_fun, params, random_mini_batch)
                 for param in params:
                     params[param] = params[param] - (lr * d_param[param]) # e.g. w = w - lr * d_w
@@ -142,41 +147,154 @@ class optimization():
         <dict>: The optimized parameters
     """ 
     def momentum(self, params:dict, lr:float, gamma:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict:
-        v_k = 0
-        d_param = {}
+        v_t = {}
+        for param in params:
+            v_t[param] = 0 # initialize v_t
+
         for i in range(epochs):
             for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
                 d_param = self.eval_grads(loss_fun, params, random_mini_batch)
                 for param in params:
-                    vk_1 = (lr * d_param[param]) + (gamma * v_k) # (gamma * v_k) is the momentum
-                    params[param] = params[param] - vk_1 # e.g. w = w - ((lr * d_w) + (gamma * vk_1))
-                    v_k = vk_1 # everything before was done for readability of the math. This line is to update the momentum var but isnt true to form for the math.  
+                    v_t[param] = (lr * d_param[param]) + (gamma * v_t[param]) # (gamma * v_t) is the momentum
+                    params[param] = params[param] - v_t[param] # e.g. w = w - ((lr * d_w) + (gamma * v_{t-1}))
                 
         return params
     
     def nesterov_gradient_acceleration(self, params:dict, lr:float, gamma:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict:
-        v_k = 0
-        d_param = {}
+        v_t = {}
+        for param in params:
+            v_t[param] = 0 # initialize v_t
+
         for i in range(epochs):
             for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
                 params_copy = params.copy()
                 for param in params_copy:
-                    params_copy[param] = params_copy[param] - (gamma * v_k)
+                    params_copy[param] = params_copy[param] - (gamma * v_t)
 
                 d_param = self.eval_grads(loss_fun, params_copy, random_mini_batch)
                 for param in params:
-                    vk_1 = (lr * d_param[param]) + (gamma * v_k) # (gamma * v_k) is the momentum
-                    params[param] = params[param] + vk_1 # e.g. w = w - ((lr * d_w) + (gamma * vk_1))
-                    v_k = vk_1 # everything before was done for readability of the math. This line is to update the momentum var but isnt true to form for the math.  
+                    v_t[param] = (lr * d_param[param]) + (gamma * v_t[param]) # (gamma * v_t) is the momentum
+                    params[param] = params[param] + v_t[param] # e.g. w = w - ((lr * d_w) + (gamma * v_{t-1}))
                 
         return params
     
-    def adagrad(self):
-        #TODO: implement adagrad
-        pass
+    def adaptive_gradient(self, params:dict, lr:float, epsilon:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict: #adagrad
+        assert(epsilon > 0)
+        g_t = {}
+        for param in params:
+            g_t[param] = 0 # initialize g_t
+
+        for i in range(epochs):
+            for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
+                d_param = self.eval_grads(loss_fun, params, random_mini_batch)
+                for param in params:
+                    g_t[param] = g_t[param] + np.pow(d_param[param], 2)
+                    epsilon = np.matmul(epsilon, np.eye(params[param].shape[0]))
+                    eta_t = lr / np.sqrt(g_t[param] + epsilon)
+                    params[param] = params[param] - eta_t * d_param[param] # e.g. param_{t+1} = param_{t} - \frac{\eta}{\sqrt{v^{param}_{t} + (\epsilon * I)}} * \nabla_{param_{t}}
+                
+        return params
+
+    def ada_delta(self, params:dict, lr:float, epsilon:float, gamma:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict:
+        assert(epsilon > 0)
+        g_t = {}
+        delta_param_t = {}
+        for param in params:
+            g_t[param] = 0 # initialize g_t
+            delta_param_t[param] = 0  # initialize delta_{param_{t}}
+
+        for i in range(epochs):
+            for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
+                d_param = self.eval_grads(loss_fun, params, random_mini_batch)
+                for param in params:
+                    g_t[param] = (gamma * g_t[param]) + ((1 - gamma) * np.pow(d_param[param], 2))
+                    delta_param_t[param] = (gamma * delta_param_t[param]) + ((1 - gamma) * np.pow(d_param[param], 2))
+                    epsilon = np.matmul(epsilon, np.eye(params[param].shape[0]))
+                    eta_t = np.sqrt(delta_param_t[param] + epsilon) / np.sqrt(g_t[param] + epsilon)
+                    params[param] = params[param] - eta_t * d_param[param] # e.g. param_{t+1} = param_{t} - \frac{\eta}{\sqrt{v^{param}_{t} + (\epsilon * I)}} * \nabla_{param_{t}}
+                
+        return params
+
+    def root_mean_square_propagation(self, params:dict, lr:float, epsilon:float, gamma:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict: # rms_prop
+        assert(epsilon > 0)
+        g_t = {}
+        for param in params:
+            g_t[param] = 0 # initialize g_t
+
+        for i in range(epochs):
+            for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
+                d_param = self.eval_grads(loss_fun, params, random_mini_batch)
+                for param in params:
+                    g_t[param] = (gamma * g_t[param]) + ((1 - gamma) * np.pow(d_param[param], 2))
+                    epsilon = np.matmul(epsilon, np.eye(params[param].shape[0]))
+                    eta_t = lr / np.sqrt(g_t[param] + epsilon)
+                    params[param] = params[param] - eta_t * d_param[param] # e.g. param_{t+1} = param_{t} - \frac{\eta}{\sqrt{v^{param}_{t} + (\epsilon * I)}} * \nabla_{param_{t}}
+                
+        return params
+
+    def adaptive_moment_estimation(self, params:dict, lr:float, epsilon:float, beta:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict: # Adam
+        assert(epsilon > 0)
+        v_t = {}
+        g_t = {}
+        for param in params:
+            v_t[param] = 0 # initialize v_t
+            g_t[param] = 0 # initialize g_t
+
+        beta_v, beta_g = beta
+
+        for i in range(epochs):
+            for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
+                d_param = self.eval_grads(loss_fun, params, random_mini_batch)
+                for param in params:
+                    v_t[param] = (beta_v * v_t[param]) + ((1 - beta_v) * np.pow(d_param[param], 2))
+                    g_t[param] = (beta_g * v_t[param]) + ((1 - beta_g) * d_param[param])
+                    g_hat = g_t[param] / (1 - (beta_g**(i+1)))
+                    v_hat = v_t[param] / (1 - (beta_v**(i+1)))
+                    epsilon = np.matmul(epsilon, np.eye(params[param].shape[0]))
+                    eta_t = lr / np.sqrt(v_hat + epsilon)
+                    params[param] = params[param] - eta_t * g_hat # e.g. param_{t+1} = param_{t} - \frac{\eta}{\sqrt{\hat{v^{param}_{t}} + (\epsilon * I)}} * \hat{g_{t}}
+                
+        return params
+
+    def adamw(self, params:dict, lr:float, wd:float, epsilon:float, beta:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict: # Adam
+        assert(epsilon > 0)
+        v_t = {}
+        g_t = {}
+        for param in params:
+            v_t[param] = 0 # initialize v_t
+            g_t[param] = 0 # initialize g_t
+
+        beta_v, beta_g = beta
+
+        for i in range(epochs):
+            for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
+                d_param = self.eval_grads(loss_fun, params, random_mini_batch)
+                for param in params:
+                    v_t[param] = (beta_v * v_t[param]) + ((1 - beta_v) * np.pow(d_param[param], 2))
+                    g_t[param] = (beta_g * v_t[param]) + ((1 - beta_g) * d_param[param])
+                    g_hat = g_t[param] / (1 - (beta_g**(i+1)))
+                    v_hat = v_t[param] / (1 - (beta_v**(i+1)))
+                    epsilon = np.matmul(epsilon, np.eye(params[param].shape[0]))
+                    eta_t = lr / np.sqrt(v_hat + epsilon)
+                    params[param] = params[param] - (eta_t * g_hat) - (lr * wd * params[param]) # e.g. param_{t+1} = param_{t} - \frac{\eta}{\sqrt{\hat{v^{param}_{t}} + (\epsilon * I)}} * \hat{g_{t}}
+                
+        return params
+
+    # ----------------------------------------------------------------------------------------------------------------------
+    # ----------------------------------------------- Second Order Algorithms ----------------------------------------------
+    # ----------------------------------------------------------------------------------------------------------------------
+
+
     
     # ----------------------------------------------------------------------------------------------------------------------
-    # --------------------------------------- Overloaded functions ---------------------------------------------------------
+    # ----------------------------------------------- Overloaded functions -------------------------------------------------
     # ----------------------------------------------------------------------------------------------------------------------
     
     """
@@ -196,10 +314,10 @@ class optimization():
     """ 
     def SGD(self, params:dict, lr:float, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict:
         size_of_batch = 1
-        d_param = {}
 
         for i in range(epochs):
             for random_data_point in self.batch_iterator(data, size_of_batch):
+                d_param = {}
                 d_param = self.eval_grads(loss_fun, params, random_data_point)
                 for param in params:
                     params[param] = params[param] - (lr * d_param[param]) # e.g. w = w - lr * d_w
@@ -223,9 +341,9 @@ class optimization():
         <dict>: The optimized parameters
     """ 
     def mini_batch_SGD(self, params:dict, lr:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict:
-        d_param = {}
         for i in range(epochs):
             for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
                 d_param = self.eval_grads(loss_fun, params, random_mini_batch)
                 for param in params:
                     params[param] = params[param] - (lr * d_param[param]) # e.g. w = w - lr * d_w
@@ -233,22 +351,82 @@ class optimization():
         return params
     
     def NAG(self, params:dict, lr:float, gamma:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict:
-        v_k = 0
-        d_param = {}
+        v_t = 0
         for i in range(epochs):
             for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
                 params_copy = params.copy()
                 for param in params_copy:
-                    params_copy[param] = params_copy[param] - (gamma * v_k)
+                    params_copy[param] = params_copy[param] - (gamma * v_t)
 
                 d_param = self.eval_grads(loss_fun, params_copy, random_mini_batch)
                 for param in params:
-                    vk_1 = (lr * d_param[param]) + (gamma * v_k) # (gamma * v_k) is the momentum
-                    params[param] = params[param] + vk_1 # e.g. w = w - ((lr * d_w) + (gamma * vk_1))
-                    v_k = vk_1 # everything before was done for readability of the math. This line is to update the momentum var but isnt true to form for the math.  
+                    vt_1 = (lr * d_param[param]) + (gamma * v_t) # (gamma * v_t) is the momentum
+                    params[param] = params[param] + vt_1 # e.g. w = w - ((lr * d_w) + (gamma * vt_1))
+                    v_t = vt_1 # everything before was done for readability of the math. This line is to update the momentum var but isnt true to form for the math.  
+                
+        return params
+
+    def adagrad(self, params:dict, lr:float, epsilon:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict: 
+        assert(epsilon > 0)
+        g_t = {}
+        for param in params:
+            g_t[param] = 0 # initialize g_t
+
+        for i in range(epochs):
+            for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
+                d_param = self.eval_grads(loss_fun, params, random_mini_batch)
+                for param in params:
+                    g_t[param] = g_t[param] + np.pow(d_param[param], 2)
+                    epsilon = np.matmul(epsilon, np.eye(params[param].shape[0]))
+                    eta_t = lr / np.sqrt(g_t[param] + epsilon)
+                    params[param] = params[param] - eta_t * d_param[param] # e.g. param_{t+1} = param_{t} - \frac{\eta}{\sqrt{v^{param}_{t} + (\epsilon * I)}} * \nabla_{param_{t}}
+                
+        return params
+
+    def rms_prop(self, params:dict, lr:float, epsilon:float, gamma:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict: 
+        assert(epsilon > 0)
+        g_t = {}
+        for param in params:
+            g_t[param] = 0 # initialize g_t
+
+        for i in range(epochs):
+            for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
+                d_param = self.eval_grads(loss_fun, params, random_mini_batch)
+                for param in params:
+                    g_t[param] = (gamma * g_t[param]) + ((1 - gamma) * np.pow(d_param[param], 2))
+                    epsilon = np.matmul(epsilon, np.eye(params[param].shape[0]))
+                    eta_t = lr / np.sqrt(g_t[param] + epsilon)
+                    params[param] = params[param] - eta_t * d_param[param] # e.g. param_{t+1} = param_{t} - \frac{\eta}{\sqrt{v^{param}_{t} + (\epsilon * I)}} * \nabla_{param_{t}}
                 
         return params
     
+    def adam(self, params:dict, lr:float, epsilon:float, beta:float, size_of_batch:int, epochs:int, loss_fun:Callable, data:np.ndarray) -> dict: 
+        assert(epsilon > 0)
+        v_t = {}
+        g_t = {}
+        for param in params:
+            v_t[param] = 0 # initialize v_t
+            g_t[param] = 0 # initialize g_t
+
+        beta_v, beta_g = beta
+
+        for i in range(epochs):
+            for random_mini_batch in self.batch_iterator(data, size_of_batch):
+                d_param = {}
+                d_param = self.eval_grads(loss_fun, params, random_mini_batch)
+                for param in params:
+                    v_t[param] = (beta_v * v_t[param]) + ((1 - beta_v) * np.pow(d_param[param], 2))
+                    g_t[param] = (beta_g * v_t[param]) + ((1 - beta_g) * d_param[param])
+                    g_hat = g_t[param] / (1 - (beta_g**(i+1)))
+                    v_hat = v_t[param] / (1 - (beta_v**(i+1)))
+                    epsilon = np.matmul(epsilon, np.eye(params[param].shape[0]))
+                    eta_t = lr / np.sqrt(v_hat + epsilon)
+                    params[param] = params[param] - eta_t * g_hat # e.g. param_{t+1} = param_{t} - \frac{\eta}{\sqrt{\hat{v^{param}_{t}} + (\epsilon * I)}} * \hat{g_{t}}
+                
+        return params
     
     
     
